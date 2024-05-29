c62fc25dd3cca4ae6b8a441e819a86272a2fbc14
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-05-29 15:54:36 +0200

    feat: all queries done exept for number 3

--- university.sql
+++ university.sql
@@ -24,4 +24,45 @@ FROM `courses`
 WHERE `year` = 1
 AND `period` = "I semestre";
 
+-- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del
+-- 20/06/2020 (21)
+SELECT * 
+FROM `exams`
+WHERE HOUR(`hour`) > 13  
+AND `date` = "2020-06-20";
+
+-- 6. Selezionare tutti i corsi di laurea magistrale (38)
+SELECT * 
+FROM `degrees`
+WHERE `level` = "magistrale"
+;
+
+-- 7. Da quanti dipartimenti è composta l'università? (12)
+SELECT COUNT(`id`) as "numero dipartimenti"
+FROM `departments`;
+
+-- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
+SELECT * 
+FROM `teachers`
+WHERE `phone` is NULL;
+
+-- 9. Inserire nella tabella degli studenti un nuovo record con i propri dati (per il campo
+-- degree_id, inserire un valore casuale)
+INSERT INTO `students` (`degree_id`,`name`,`surname`,`date_of_birth`,`fiscal_code`,`enrolment_date`,`registration_number`,`email`)
+VALUES (25, "Eugenio","Malavasi","1991-04-02","MLVGNE91D02F240O","2024-05-29",666666,"eugenio@gmail.com")
+;
+
+-- 10. Cambiare il numero dell’ufficio del professor Pietro Rizzo in 126
+UPDATE `teachers`
+SET `office_number` = 126
+WHERE `name` = "Pietro" AND `surname` = "Rizzo"
+;
+
+-- 11. Eliminare dalla tabella studenti il record creato precedentemente al punto 9
+DELETE FROM `students` 
+WHERE `name` = "eugenio" AND `surname` = "malavasi"
+;
+
+
+
 
