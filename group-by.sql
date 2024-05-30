95be4d8064ab3b4682b7f4904bf1be86e9039039
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-05-30 15:46:08 +0200

    feat: group by queries done

---
+++ group-by.sql
@@ -0,0 +1,28 @@
+-- 1. Contare quanti iscritti ci sono stati ogni anno
+SELECT YEAR(`students`.`enrolment_date`) AS `year`, COUNT(`students`.`id`) as `students_number`
+FROM `students` 
+GROUP BY YEAR(`students`.`enrolment_date`);
+
+-- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+SELECT `teachers`.`office_address`, COUNT(`teachers`.`id`) as `number_of_teachers:`
+FROM `teachers`
+GROUP BY `teachers`.`office_address`;
+
+-- 3. Calcolare la media dei voti di ogni appello d'esame
+SELECT `degrees`.`name`, AVG(`exam_student`.`vote`) AS average_vote
+FROM `exams`
+INNER JOIN `exam_student`
+ON `exams`.`id` = `exam_student`.`exam_id`
+INNER JOIN `courses`
+ON `courses`.`id` = `exams`.`course_id`
+INNER JOIN `degrees`
+ON `degrees`.`id` = `courses`.`degree_id`
+GROUP BY `degrees`.`name`;
+
+-- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+
+SELECT `departments`.`name` as `department_name`, COUNT(`degrees`.`id`) as `degrees_number_per_dep`
+FROM `departments`
+INNER JOIN `degrees`
+ON `departments`.`id` = `degrees`.`department_id`
+GROUP BY `departments`.`name`;
--- university.sql
+++ university.sql
@@ -17,18 +17,16 @@ FROM `students`
 WHERE TIMESTAMPDIFF(YEAR, `date_of_birth`, CURRENT_DATE) > 30  
 ;
 
--- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di
--- laurea (286)
+-- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 SELECT * 
 FROM `courses`
 WHERE `year` = 1
 AND `period` = "I semestre";
 
--- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del
--- 20/06/2020 (21)
+-- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 SELECT * 
 FROM `exams`
-WHERE HOUR(`hour`) > 13  
+WHERE HOUR(`hour`) >= 14  
 AND `date` = "2020-06-20";
 
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
@@ -46,8 +44,7 @@ SELECT *
 FROM `teachers`
 WHERE `phone` is NULL;
 
--- 9. Inserire nella tabella degli studenti un nuovo record con i propri dati (per il campo
--- degree_id, inserire un valore casuale)
+-- 9. Inserire nella tabella degli studenti un nuovo record con i propri dati (per il campo degree_id, inserire un valore casuale)
 INSERT INTO `students` (`degree_id`,`name`,`surname`,`date_of_birth`,`fiscal_code`,`enrolment_date`,`registration_number`,`email`)
 VALUES (25, "Eugenio","Malavasi","1991-04-02","MLVGNE91D02F240O","2024-05-29",666666,"eugenio@gmail.com")
 ;
