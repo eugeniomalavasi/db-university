833b2e039277f23b4b0740a22d8d77ff95e89f59
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-05-30 16:52:42 +0200

    feat: 6 join queries completed

--- join.sql
+++ join.sql
@@ -37,3 +37,32 @@ ON `degrees`.`id` = `students`.`degree_id`
 INNER JOIN `courses`
 ON `degrees`.`id` = `courses`.`degree_id`  
 ORDER BY `students`.`name`, `students`.`surname` ASC;
+
+-- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+-- DEGREES NAME | COURSES NAME | TEACHER NAME | TEACHER SURNAME
+SELECT `degrees`.`name` AS `degree_name`, `courses`.`name` AS `course_name`, `teachers`.`name` AS `teachers_name`, `teachers`.`surname` AS `teachers_surname`  
+FROM `degrees`
+INNER JOIN `courses`
+ON `degrees`.`id` = `courses`.`degree_id`
+INNER JOIN `course_teacher`
+ON `courses`.`id` = `course_teacher`.`course_id`
+INNER JOIN `teachers`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`  
+ORDER BY `degree_name` ASC
+
+-- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+SELECT `teachers`.`name`, `teachers`.`surname`, `departments`.`name` AS `department_name`
+FROM `teachers` 
+INNER JOIN `course_teacher`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
+INNER JOIN `courses`
+ON `courses`.`id` = `course_teacher`.`course_id`
+INNER JOIN `degrees`
+ON `degrees`.`id` = `courses`.`degree_id`
+INNER JOIN `departments`
+ON `departments`.`id` = `degrees`.`department_id`
+WHERE `departments`.`name` = "dipartimento di matematica"  
+ORDER BY `teachers`.`surname` ASC;
+
+-- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per
+-- superare ciascuno dei suoi esami
